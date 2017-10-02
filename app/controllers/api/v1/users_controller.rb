71809fe2ffa73dd9773d57d40a45423c1749ccd3
bleavs <bleavitt@colgate.edu>
2017-10-02 17:37:46 -0400

    extended routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,9 @@
 class Api::V1::UsersController < ApplicationController
-  
+
+  def create
+    @user = User.find_or_create_by(username: body)
+    render json: @user
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :login, only: [:create]
+
       resources :users, only: [:create]
+
+      resources :locations, only: [:create]
     end
   end# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
